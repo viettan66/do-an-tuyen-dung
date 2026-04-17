4fa098a174a16c736d2cae5fa913e7749393a823
tannguyen2 <109942469+tannguyen2@users.noreply.github.com>
2026-04-18 00:02:45 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -39,7 +39,6 @@ builder.Services.AddScoped(sp =>
 });
 
 // Cookie authentication
-var isDevelopment = app.Environment.IsDevelopment();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
@@ -49,7 +48,7 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
         options.LoginPath = "/login";
         options.Cookie.HttpOnly = true;
         options.Cookie.SameSite = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
-        options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.None;
+        options.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.SameAsRequest;
         options.Cookie.IsEssential = true;
     });
 
